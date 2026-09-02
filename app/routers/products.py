ea8eefb1f49f39f41e9a3a3566dd7baf025cbafa
sirsasha <krypn87@mail.ru>
2026-09-03 01:19:54 +0800

    feat(products): add endpoint POST /products/

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import select
+
+from app.db_depends import SessionDep
+from app.models import Category as CategoryModel
+from app.models import Product as ProductModel
+from app.schemas import Product as ProductSchema
+from app.schemas import ProductCreate
 
 router = APIRouter(
     prefix="/products",
@@ -13,11 +20,26 @@ async def get_all_products() -> dict[str, str]:
     return {"message": "Список всех товаров (заглушка)"}
 
 
-@router.post("/")
-async def create_product() -> dict[str, str]:
+@router.post("/", response_model=ProductSchema, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductCreate, db: SessionDep) -> ProductModel:
     """Создаёт новый товар."""
 
-    return {"message": "Товар создан (заглушка)"}
+    stmt = select(CategoryModel).where(
+        CategoryModel.id == product.category_id, CategoryModel.is_active.is_(True)
+    )
+    category = db.scalars(stmt).first()
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Категория не найдена или неактивна",
+        )
+
+    db_product = ProductModel(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
 
 
 @router.get("/category/{category_id}")
